85df5cdb12f90b9e868e32fd72d477fb27d80d38
Azhin Zarei <a.zarei@fwutech.com>
2021-09-24 20:05:35 +0330

    Add project files.

---
+++ Services/Ordering/Ordering.API/Mapping/OrderingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Linq;
+
+namespace Ordering.API.Mapping
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    //In this class we collect all the validations 
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        //Collecting all validator including checkout order and update order command
+        //Fluent validator looking for abstract validator and abstract validator implement IValidator so we are seeking IValidator object and these two calsses
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                //In this line of code ValidateAsync check the all validation of the command 
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
